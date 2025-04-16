7b475441592da1dda79813fa0eb07931b6b14ce3
Eze1504 <eze.sarabia04@gmail.com>
2025-04-16 18:20:52 -0300

    Agregado de todos los test realizados ya que debido a hacer mal los PR se superponian los codigos

--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -11,9 +11,8 @@ class TestPalindrome(unittest.TestCase):
         self.assertTrue(is_palindrome("rayar"))
         self.assertTrue(is_palindrome("oso"))
         self.assertTrue(is_palindrome("salas"))
-
+    
     def test_phrase_palindromes(self):
-       
         self.assertTrue(is_palindrome("A man, a plan, a canal: Panama"))
         self.assertTrue(is_palindrome("Was it a car or a cat I saw?"))
         self.assertTrue(is_palindrome("No lemon, no melon"))
@@ -22,24 +21,23 @@ class TestPalindrome(unittest.TestCase):
         self.assertTrue(is_palindrome("La ruta nos aportó otro paso natural"))
         self.assertTrue(is_palindrome("Yo hago yoga hoy"))
         self.assertTrue(is_palindrome("Amo la pacífica paloma"))
-
+    
     def test_frase_vacia(self):
         with self.assertRaises(ValueError):
-            es_palindromo("")
-
+            is_palindrome("")  
+    
     def test_un_solo_caracter(self):
-        self.assertTrue(es_palindromo("a"))
-
+        self.assertTrue(is_palindrome("a"))  
+    
     def test_solo_espacios(self):
         with self.assertRaises(ValueError):
-            es_palindromo("     ")
-
+            is_palindrome("     ")  
+    
     def test_caracteres_especiales(self):
-        self.assertTrue(es_palindromo("¿Acaso hubo búhos acá?"))
-
+        self.assertTrue(is_palindrome("¿Acaso hubo búhos acá?"))  
+    
     def test_mayusculas_acentos_y_puntuacion(self):
-        self.assertTrue(es_palindromo("Ánita, la Va LavÁ!"))
-
+        self.assertFalse(is_palindrome("Ánita, la Va LavÁ!"))  
 
 if __name__ == '__main__':
     unittest.main()
